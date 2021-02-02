ab256007bc90127c0381e80c13116ffbc46218c9
SigurdJanson <emailjs@web.de>
2021-02-02 23:20:22 +0100

    Package documentation

---
+++ R/plainEnum-package.R
@@ -0,0 +1,12 @@
+#' @details
+#' An enumeration type (or enum type) is a value type defined by a set of named constants
+#' of the underlying integral numeric type. To define an enumeration type, use
+#' [enum()] and specify the names and values of enum members.
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
